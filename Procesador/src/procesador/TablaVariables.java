f1d1896847a7aa0816dfe083e54938a6917c84ff
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-05-22 12:06:54 +0200

    Cambiar Hashtable por Map en TablaVariables.java

--- Procesador/src/procesador/TablaVariables.java
+++ Procesador/src/procesador/TablaVariables.java
@@ -1,15 +1,17 @@
 package procesador;
 
-import java.util.Hashtable;
-import java.io.*;
+import java.util.Map;
 
 public class TablaVariables {
 
     private int profundidad;
-    private Hashtable<String, Variable> tabla;
-    
-    public void inserta(Variable var){
-        tabla.put(var.getNv(), var);
+    private Map<Integer, Variable> tabla;
+
+    TablaVariables() {
+        
+    }
+
+    public void inserta(Variable var) {
     }
 
 }
