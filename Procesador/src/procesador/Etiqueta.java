a6394f1ddfedb42e9a106092d195dbc704f77ec4
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-05-31 22:29:05 +0200

    Cambiar el constructor de Etiqueta

--- Procesador/src/procesador/Etiqueta.java
+++ Procesador/src/procesador/Etiqueta.java
@@ -2,21 +2,29 @@ package procesador;
 
 /**
  * Etiqueta. Clase que sirve para gestionar las etiquetas que se generan.
+ * 
+ * @author Gian Lucas Martín Chamorro
  */
 public class Etiqueta {
     private int nl; // Número de línea
     private int ne; // Número de etiqueta
     private static int ce = 0; // Cantidad de etiquetas
 
+    public Etiqueta(int nl) {
+        ce++; // Aumenta la cantidad de etiquetas generadas
+        this.nl = nl;
+        ne = ce; // Asigna el número de etiqueta
+    }
+
+    public int getNe() {
+        return ne;
+    }
+
     public int getNl() {
         return nl;
     }
 
     public void setNl(int nl) {
-        this.nl = nl;
-    }
-
-    public Etiqueta(int nl) {
         this.nl = nl;
     }
 }
