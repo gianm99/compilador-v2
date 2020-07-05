7ec587e027686d58f9edfa64c00a5a93a0bea232
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-07-05 16:45:35 +0200

    Añadir atributos

--- Procesador/src/procesador/Variable.java
+++ Procesador/src/procesador/Variable.java
@@ -1,7 +1,8 @@
 package procesador;
 
 /**
- * Variable. Clase que sirve para gestionar las variables que aparecen en el código fuente.
+ * Variable. Clase que sirve para gestionar las variables que aparecen en el
+ * código fuente.
  * 
  * @author Gian Lucas Martín Chamorro
  */
@@ -10,22 +11,41 @@ public class Variable {
     private int nv; // Número de variable
     private Procedimiento proc; // Procedimiento que la ha declarado
     private Simbolo.Tipo tipo; // Tipo: variable, constante o argumento
+    private Simbolo.TSub tsub; // Tipo subyacente
     private boolean temporal;
+    private static boolean inicializada;
     private static int cv = 0; // Cantidad de variables creadas
 
-    public Variable(Procedimiento proc, Simbolo.Tipo tipo) {
+    public Variable(Variable v) {
+        this.r = v.r;
+        this.nv = v.nv;
+        this.proc = v.proc;
+        this.tipo = v.tipo;
+        this.temporal = v.temporal;
+    }
+
+    public Variable(Procedimiento proc, Simbolo.Tipo tipo, Simbolo.TSub tsub) {
         cv++; // Aumenta la cantidad de variables
         this.nv = cv;
         this.tipo = tipo;
+        this.setTsub(tsub);
         this.proc = proc;
     }
 
-    public Variable(Variable v) {
-        this.r=v.r;
-        this.nv=v.nv;
-        this.proc=v.proc;
-        this.tipo=v.tipo;
-        this.temporal=v.temporal;
+    public Simbolo.TSub getTsub() {
+        return tsub;
+    }
+
+    public void setTsub(Simbolo.TSub tsub) {
+        this.tsub = tsub;
+    }
+
+    public static boolean isInicializada() {
+        return inicializada;
+    }
+
+    public static void setInicializada(boolean inicializada) {
+        Variable.inicializada = inicializada;
     }
 
     public boolean isTemporal() {
@@ -71,9 +91,9 @@ public class Variable {
     @Override
     public String toString() {
         if (temporal) {
-            return "t"+nv;
+            return "t" + nv;
         } else {
-            return "v"+nv;
+            return "v" + nv;
         }
     }
 
