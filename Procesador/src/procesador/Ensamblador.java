dfba1efc3ae1d5d55d9c1db3a3ce80658dd6a3be
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-07-05 12:49:08 +0200

    Quitar el modificador static

--- Procesador/src/procesador/Ensamblador.java
+++ Procesador/src/procesador/Ensamblador.java
@@ -8,18 +8,25 @@ import java.io.Writer;
 import java.util.ArrayList;
 
 public class Ensamblador {
+    private String directorio;
+    private ArrayList<Instruccion> c3d;
 
-    public static void ensamblar(String directorio, ArrayList<Instruccion> c3d) {
-        generarASM(directorio, c3d);
-        generarEXE(directorio);
+    public Ensamblador(String directorio, ArrayList<Instruccion> c3d) {
+        this.directorio = directorio;
+        this.c3d = c3d;
     }
 
-    public static void generarEXE(String directorio) {
+    public void ensamblar() {
+        generarASM();
+        generarEXE();
+    }
+
+    public void generarEXE() {
         try {
             Process compilado = Runtime.getRuntime().exec("ml /c /Zd /coff " + directorio + ".asm");
             compilado.waitFor();
-            Process enlazado =
-                    Runtime.getRuntime().exec("link /subsystem:console " + directorio + ".obj");
+            Process enlazado = Runtime.getRuntime()
+                    .exec("link /subsystem:console " + directorio + ".obj");
             enlazado.waitFor();
             System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT
                     + "Proceso de ensamblado completado con éxito\n"
@@ -30,10 +37,10 @@ public class Ensamblador {
         }
     }
 
-    public static void generarASM(String directorio, ArrayList<Instruccion> c3d) {
+    public void generarASM() {
         Writer buffer;
         File asmFile = new File(directorio + ".asm");
-        ArrayList<String> asm = traducir(c3d);
+        ArrayList<String> asm = traducir();
         try {
             buffer = new BufferedWriter(new FileWriter(asmFile));
             for (int i = 0; i < asm.size(); i++) {
@@ -44,18 +51,84 @@ public class Ensamblador {
         }
     }
 
-    public static ArrayList<String> traducir(ArrayList<Instruccion> c3d) {
+    public ArrayList<String> traducir() {
         ArrayList<String> asm = new ArrayList<>();
         asm.add(".386");
         asm.add(".model flat, stdcall");
-        asm.add("option casemap: none");
+        asm.add("option casemap:none");
         asm.add("include \\masm32\\include\\windows.inc");
         asm.add("include \\masm32\\include\\kernel32.inc");
         asm.add("include \\masm32\\include\\masm32.inc");
         asm.add("includelib \\masm32\\lib\\kernel32.lib");
         asm.add("includelib \\masm32\\lib\\masm32.lib");
-        asm.add("");
-        // TODO Hacer la traducción a x86
+        asm.add(".data"); // Datos inicializados
+        asm.add(".data?"); // Datos no inicializados
+        asm.add(".const"); // Todas las constantes
+        asm.add(".code");
+        // TODO Declarar aquí todas las subrutinas
+        asm.add("start:");
+        for (Instruccion instruccion : c3d) {
+            switch (instruccion.getCodigo()) {
+            case EQ:
+                break;
+            case GE:
+                break;
+            case GT:
+                break;
+            case LE:
+                break;
+            case LT:
+                break;
+            case NEQ:
+                break;
+            case add:
+                break;
+            case and:
+                break;
+            case call:
+                break;
+            case copy:
+                break;
+            case div:
+                break;
+            case et:
+                break;
+            case ifEQ:
+                break;
+            case ifGE:
+                break;
+            case ifGT:
+                break;
+            case ifLE:
+                break;
+            case ifLT:
+                break;
+            case ifNE:
+                break;
+            case init:
+                break;
+            case jump:
+                break;
+            case mult:
+                break;
+            case neg:
+                break;
+            case not:
+                break;
+            case or:
+                break;
+            case params:
+                break;
+            case ret:
+                break;
+            case sub:
+                break;
+            default:
+                break;
+
+            }
+        }
+        asm.add("end start");
         return asm;
     }
 }
